004e237a486c45d675cab9a4827830c8037bbd16
Paz <mariapaz.abarca@toctoc.com>
2021-07-31 00:10:03 -0400

    Add states to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import CommitCard from './components/CommitCard';
+import React, {useState} from 'react';
+import { fetchAPIGitHub } from './API';
+// import CommitCard from './components/CommitCard';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [commits, setCommits] = useState([]);
+
+  console.log(fetchAPIGitHub());
+  
   return (
     <div className="App">
       <h1>Dashboard</h1>
-      {/* <CommitCard/>       */}
+      {/* {commits.map(commit => (
+        <CommitCard 
+          nameComitter = {commit.commit.committer.name} 
+          dateCommit = {commit.commit.committer.date} 
+          messageCommit = {commit.commit.message}  
+          urlCommit = {commit.commit.url} 
+          idCommit = {commit.sha} 
+        />  
+      ))}     */}
     </div>
   );
 }
