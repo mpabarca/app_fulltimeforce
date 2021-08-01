abb823c9c5d35ced7cd8d472e9c5051bb15c65f0
Paz <mariapaz.abarca@toctoc.com>
2021-08-01 12:17:50 -0400

    Add types to method fetchAPIGithub

--- src/API.ts
+++ src/API.ts
@@ -1,6 +1,25 @@
+export type CommitData = {
+    sha: string
+    node_id: string
+    commit: {
+        author: {
+            name: string
+            email: string
+            date: string
+        }
+        message: string
+    }
+    url: string
+    author: {
+        login: string
+        avatar_url: string
+        html_url: string
+    }
+    
+}
 
-export const fetchAPIGitHub = async () => {
+export const fetchAPIGitHub = async (): Promise<CommitData[]> => {
     const endpoint = "https://api.github.com/repos/mpabarca/app_fulltimeforce/commits";
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data;
 };
